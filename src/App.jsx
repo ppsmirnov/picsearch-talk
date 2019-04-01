db11b207f642405206dd8f4aebcd115ccc3bf6a8
Pavel Smirnov <smirnovpp@yandex-team.ru>
2019-04-01 12:18:54 +0300

    Add deploy scripts

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Route, Link } from "react-router-dom";
 
 import CycleBlockingDemo from "./pages/CycleBlockingDemo";
 import BlockingImageDemo from "./pages/BlockingImageDemo";
